64e91d41921cf3c209e6d5874317d4b51bdbb8c6
King. YPC <18864628965@163.com>
2023-08-06 19:56:51 +0800

    context测试1

--- ContextLearn/contextLearn.go
+++ ContextLearn/contextLearn.go
@@ -1,1 +1,96 @@
-package ContextLearn
+package main
+
+const KeyID = "123456"
+
+func main() {
+
+}
+
+/*
+// WaitGroup的用法
+func main() {
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		fmt.Println("Task 1")
+	}()
+	go func() {
+		defer wg.Done()
+		fmt.Println("Task 2")
+	}()
+
+	wg.Wait()
+	fmt.Println("Over!")
+}
+
+*/
+
+/*
+// WithTimeout,请求超时自动取消
+func main() {
+	parent := context.Background()
+	ctx, _ := context.WithTimeout(parent, 1*time.Millisecond)
+
+	start := time.Now()
+	req, _ := http.NewRequest(http.MethodGet, "http://www.google.com", nil)
+	req.WithContext(ctx)
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Request failed:", err, " time ", time.Now().Sub(start))
+		return
+	}
+	fmt.Println("Response received, status code:", res.StatusCode)
+}
+*/
+
+/*
+func main() {
+	exit := make(chan string)
+	parent := context.Background()
+	ctx, cancel := context.WithCancel(parent)
+	go worker(ctx, "01", exit)
+	go worker(ctx, "02", exit)
+	go worker(ctx, "03", exit)
+	time.Sleep(time.Second * 5)
+	cancel()
+	<-exit
+}
+
+// CancelCtx取消同级及其下级的goroutine
+func worker(ctx context.Context, name string, exit chan string) {
+	for {
+		select {
+		case <-ctx.Done():
+			defer close(exit)
+			fmt.Printf("%s closer.\n", name)
+			return
+		default:
+			fmt.Printf("%s is working.\n", name)
+			time.Sleep(time.Second * 1)
+		}
+	}
+}
+*/
+
+// ValueContext值传递
+/*
+func ContextWithValue() {
+	rand.Seed(time.Now().Unix())
+	ctx := context.WithValue(context.Background(), KeyID, rand.Int())
+	operation1(ctx)
+}
+
+func operation1(ctx context.Context) {
+	fmt.Println("operation1 for id:", ctx.Value(KeyID), " completed")
+	operation2(ctx)
+}
+
+func operation2(ctx context.Context) {
+	fmt.Println("operation2 for id:", ctx.Value(KeyID), " completed")
+}
+
+*/
--- GoroutineLearn/goroutineLearn.go
+++ GoroutineLearn/goroutineLearn.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"sync"
 	"time"
 )
 
@@ -68,30 +69,32 @@ func main() {
 
 	// 无缓存，同步通信，双向信道
 	ch := make(chan string)
-	exit := make(chan string)
+	// exit := make(chan string)
 
-	go animalCat(animal1, ch, exit)
-	go animalDog(animal2, ch)
-
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go animalCat(animal1, ch, &wg)
+	go animalDog(animal2, ch, &wg)
+	wg.Wait()
 	// 用于阻塞主线程，使得协程能够运行。当协程运行完毕，close(exit)，协程输出数据，主线程结束
-	<-exit
+	// <-exit
 
 }
 
-func animalCat(animal1 Animal, ch chan string, exit chan string) {
+func animalCat(animal1 Animal, ch chan string, wg *sync.WaitGroup) {
 	for {
 		if v, ok := <-ch; ok {
 			fmt.Println("Cat: " + v)
 			ch <- animal1.info()
 		} else {
-			defer close(exit)
+			defer wg.Done()
 			fmt.Println("Cat: Bye!")
 			return
 		}
 	}
 }
 
-func animalDog(animal2 Animal, ch chan string) {
+func animalDog(animal2 Animal, ch chan string, wg *sync.WaitGroup) {
 	ch <- animal2.info()
 	for {
 		if v, ok := <-ch; ok {
@@ -100,6 +103,7 @@ func animalDog(animal2 Animal, ch chan string) {
 			fmt.Println("Dog: Bye!")
 			close(ch)
 		} else {
+			wg.Done()
 			return
 		}
 	}
