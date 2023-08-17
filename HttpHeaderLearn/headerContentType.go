e33511f9892717e8c92611ece86e9f2fa4a2fe13
King. YPC <18864628965@163.com>
2023-08-17 17:07:55 +0800

    httpHeader1_2

--- HttpHeaderLearn/headerContentType.go
+++ HttpHeaderLearn/headerContentType.go
@@ -5,14 +5,21 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"os"
 )
+
+type Info struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+	Sex  string `json:"sex"`
+}
 
 func main() {
 	router := mux.NewRouter()
 
 	testRouter := router.Host("s3.test.com").PathPrefix("/").Subrouter()
 	testRouter.Methods("POST").Path("/form").HandlerFunc(xWwwFormUrlencoded)
-	testRouter.Methods("GET").Path("/get").HandlerFunc(getTest)
+	testRouter.Methods("POST").Path("/formdata").HandlerFunc(formData)
 
 	server := &http.Server{
 		Addr:    ":8100",
@@ -25,21 +32,75 @@ func main() {
 	}
 }
 
-// content-type: x-www-form-urlencoded
+// 一、
+// content-type: application/x-www-form-urlencoded
 // 将请求参数放在Request body中，如果是中文或特殊字符如"/"、","、“:" 等会自动进行URL转码。
 // 不支持文件，一般用于表单提交，即POST请求方式
 func xWwwFormUrlencoded(w http.ResponseWriter, r *http.Request) {
-	// 获取body的长度
-	bodyLen := r.ContentLength
-	// 字节切片
-	body := make([]byte, bodyLen)
-	// 获取request body
-	r.Body.Read(body)
-	// 输出request body
-	fmt.Fprintln(w, "body: "+string(body))
 
+	/*
+		// 一、解析request
+		log.Printf("Get %s request. host: %s\n", r.Method, r.Host)
+		// 获取body的长度
+		contentLen := r.ContentLength
+		rtype := r.Header.Get("Content-Type")
+		// 定义字节切片
+		content := make([]byte, contentLen)
+		// 获取request body
+		r.Body.Read(content)
+		fmt.Fprintln(w, "request-type: "+rtype)
+		fmt.Fprintln(w, "request-lenght: "+strconv.FormatInt(contentLen, 10))
+		fmt.Fprintln(w, "body: "+string(content))
+	*/
+
+	// 二、解析body为map类型。针对application/x-www-form-urlencoded类型
+	r.ParseForm()
+	params := r.Form
+	fmt.Fprintln(w, params)
 }
 
-func getTest(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "getTest")
+// 二、
+// content-type：multipart/form-data
+// 首先生成了一个 boundary 用于分割不同的字段，在请求实体里每个参数以------boundary开始，然后是附加信息和参数名，
+// 然后是空行，最后是参数内容。多个参数将会有多个boundary块。如果参数是文件会有特别的文件域。最后以------boundary–为结束标识。
+// multipart/form-data支持文件上传的格式，一般需要上传文件的表单则用该类型。
+func formData(w http.ResponseWriter, r *http.Request) {
+	// 一、解析body为map类型。针对multipart/form-data类型
+	r.ParseMultipartForm(128)
+	params := r.MultipartForm
+
+	// 获取普通类型的参数
+	values := params.Value
+	for key, value := range values {
+		fmt.Fprint(w, key+": ")
+		fmt.Fprintln(w, value)
+	}
+
+	// 获取file类型的参数
+	picFiles := params.File
+	fmt.Fprintln(w, picFiles)
+	for fileName, _ := range picFiles {
+		file, fileHeader, err := r.FormFile(fileName)
+		fmt.Fprintln(w, file)
+		fmt.Fprintln(w, fileHeader)
+		if err != nil {
+			log.Fatalln("文件上传有误")
+			return
+		}
+
+		fileBuffer := make([]byte, fileHeader.Size)
+		_, err = file.Read(fileBuffer)
+		if err != nil {
+			log.Fatalln("读取文件数据有误")
+			return
+		}
+
+		copyFile, _ := os.OpenFile("./copyFile/"+fileHeader.Filename, os.O_CREATE|os.O_RDWR, 0777)
+		_, err = copyFile.Write(fileBuffer)
+		if err != nil {
+			log.Fatalln()
+			return
+		}
+		defer copyFile.Close()
+	}
 }
